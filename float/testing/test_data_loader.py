d638b081488311b91d1f882125aba1017d34974f
Effi Tramountani <effi.tramountani@gmail.com>
2021-05-27 14:47:33 +0200

    Add skeleton for TestConceptDriftDetector class.

--- float/testing/test_data_loader.py
+++ float/testing/test_data_loader.py
@@ -9,18 +9,6 @@ class TestDataLoader(unittest.TestCase):
         super().__init__(method_name)
         self.data_loader = DataLoader(file_path='../data/datasets/spambase.csv', target_col=0)
 
-    def test_evaluate_input(self):
-        with self.assertRaises(AttributeError, msg='passing no inputs'):
-            DataLoader()
-        with self.assertRaises(AttributeError, msg='passing non .csv file'):
-            DataLoader(file_path='../data/__init__.py')
-        with self.assertRaises(AttributeError, msg='passing non int value for target_col'):
-            DataLoader(file_path='../data/datasets/spambase.csv', target_col=None)
-        with self.assertRaises(FileNotFoundError, msg='file not found'):
-            DataLoader(file_path='../data/datasets/spam_base.csv')
-        with self.assertRaises(ValueError, msg='non compatible .csv file'):
-            DataLoader(file_path='../../docs/time_tracking.csv')
-
     def test_init(self):
         self.assertIsInstance(self.data_loader.stream, Stream, msg='attribute stream of type Stream')
 
@@ -32,3 +20,15 @@ class TestDataLoader(unittest.TestCase):
         self.assertIsInstance(samples[1], np.ndarray, msg='returns tuple of numpy arrays')
         self.assertEqual(samples[0].shape, (n_samples, self.data_loader.stream.n_features), msg='returns data in correct shape')
         self.assertEqual(samples[1].shape, (n_samples,), msg='returns target values in correct shape')
+
+    def test_check_input(self):
+        with self.assertRaises(AttributeError, msg='passing no inputs'):
+            DataLoader()
+        with self.assertRaises(AttributeError, msg='passing non .csv file'):
+            DataLoader(file_path='../data/__init__.py')
+        with self.assertRaises(AttributeError, msg='passing non int value for target_col'):
+            DataLoader(file_path='../data/datasets/spambase.csv', target_col=None)
+        with self.assertRaises(FileNotFoundError, msg='file not found'):
+            DataLoader(file_path='../data/datasets/spam_base.csv')
+        with self.assertRaises(ValueError, msg='non compatible .csv file'):
+            DataLoader(file_path='../../docs/time_tracking.csv')
