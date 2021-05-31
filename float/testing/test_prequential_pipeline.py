8895be017dd64b7d0567f70133e323d37f08d48d
Effi Tramountani <effi.tramountani@gmail.com>
2021-05-31 19:09:20 +0200

    Add TestTimeMetric. Add skeleton for TestPrequentialPipeline.

---
+++ float/testing/test_prequential_pipeline.py
@@ -0,0 +1,17 @@
+import unittest
+from float.pipeline.prequential_pipeline import PrequentialPipeline
+
+
+class TestPrequentialPipeline(unittest.TestCase):
+    def __init__(self, method_name):
+        super().__init__(method_name)
+        self.prequential_pipeline = PrequentialPipeline()
+
+    def test_init(self):
+        pass
+
+    def test_run(self):
+        pass
+
+    def test_test_then_train(self):
+        pass
