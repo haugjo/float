3ef1a800251516a6f077717fb22fcb937f18ce0c
Effi Tramountani <effi.tramountani@gmail.com>
2021-06-23 10:43:20 +0200

    Add _draw_top_features_plot to Visualizer.

--- float/visualization/visualizer.py
+++ float/visualization/visualizer.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 from matplotlib.axes import Axes
+import numpy as np
 
 
 # TODO figure out how to check if measures can be visualized with that specific plot
@@ -7,7 +8,7 @@ class Visualizer:
     """
     Class for creating plots to visualize information.
     """
-    def __init__(self, measures, fig_size):
+    def __init__(self, measures, fig_size=(10, 5)):
         """
         Initialize the visualizer using a uniform style.
 
@@ -17,6 +18,7 @@ class Visualizer:
         """
         self.measures = measures
         self.fig_size = fig_size
+        self.font_size = 12
 
     def plot(self):
         """
@@ -49,4 +51,28 @@ class Visualizer:
         """
         fig, ax = plt.subplots(figsize=self.fig_size)
         ax.bar(range(len(self.measures)), self.measures)
+        return ax
+
+    def _draw_top_features_plot(self, feature_names):
+        """
+        Draws the most selected features over time.
+
+        Args:
+            feature_names (list): the list of feature names
+
+        Returns:
+            Axes: the Axes object containing the bar plot
+        """
+        fig, ax = plt.subplots(figsize=self.fig_size)
+        ax.grid(True, axis='y')
+        y = [feature for features in self.measures for feature in features]
+        counts = np.bincount(y)
+        top_ftr_idx = counts.argsort()[-10:][::-1]
+        ax.bar(np.arange(10), counts[top_ftr_idx], width=0.3, zorder=100)
+        ax.set_xticklabels(np.asarray(feature_names)[top_ftr_idx])
+        ax.set_ylabel('Times Selected', size=self.font_size, labelpad=1.5)
+        ax.set_xlabel('Top 10 Features', size=self.font_size, labelpad=1.6)
+        ax.tick_params(axis='both', labelsize=self.font_size * 0.7, length=0)
+        ax.set_xticks(np.arange(10))
+        ax.set_xlim(-0.2, 9.2)
         return ax
