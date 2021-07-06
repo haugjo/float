e1026539232a74d61610e9246b2bd25d73a02305
Effi Tramountani <effi.tramountani@gmail.com>
2021-07-06 17:47:23 +0200

    Add functionality to plot multiple measures in the top features and selected features plots.

--- float/sandbox.py
+++ float/sandbox.py
@@ -5,23 +5,28 @@ import matplotlib.pyplot as plt
 from float import *
 
 data_loader = data.DataLoader(None, 'data/datasets/spambase.csv', 0)
-feature_selector = feature_selection.ofs.OFS(n_total_features=data_loader.stream.n_features, n_selected_features=10,
-                                             nogueira_window_size=10)
+feature_selectors = [feature_selection.ofs.OFS(n_total_features=data_loader.stream.n_features, n_selected_features=10,
+                                               nogueira_window_size=10),
+                     feature_selection.fires.FIRES(n_total_features=data_loader.stream.n_features,
+                                                   n_selected_features=10, classes=data_loader.stream.target_values,
+                                                   nogueira_window_size=10)]
 adwin = ADWIN(delta=2)
 concept_drift_detector = concept_drift_detection.SkmultiflowDriftDetector(adwin)
 perceptron = PerceptronMask()
 predictor = prediction.skmultiflow_perceptron.SkmultiflowPerceptron(perceptron, data_loader.stream.target_values)
 
-pipeline = pipeline.prequential_pipeline.PrequentialPipeline(data_loader, feature_selector, concept_drift_detector,
-                                                             predictor, max_n_samples=data_loader.stream.n_samples)
-pipeline.run()
+for feature_selector in feature_selectors:
+    pipeline.prequential_pipeline.PrequentialPipeline(data_loader, feature_selector, concept_drift_detector,
+                                                      predictor, max_n_samples=data_loader.stream.n_samples).run()
 
-visualizer = visualization.visualizer.Visualizer([predictor.accuracy_scores, predictor.precision_scores, predictor.f1_scores], ['Accuracy', 'Precision', 'F1'], 'prediction')
+visualizer = visualization.visualizer.Visualizer(
+    [predictor.accuracy_scores, predictor.precision_scores, predictor.f1_scores], ['Accuracy', 'Precision', 'F1'],
+    'prediction')
 visualizer.scatter(plot_title='Predictor')
 plt.show()
 
-visualizer = visualization.visualizer.Visualizer(feature_selector.selection, [], 'feature_selection')
-visualizer.draw_top_features(data_loader.stream.feature_names)
+visualizer = visualization.visualizer.Visualizer([feature_selectors[0].selection, feature_selectors[1].selection], ['OFS', 'FIRES'], 'feature_selection')
+visualizer.draw_top_features(data_loader.stream.feature_names, fig_size=(15, 5))
 plt.show()
-visualizer.draw_selected_features()
+visualizer.draw_selected_features(fig_size=(10, 8))
 plt.show()
--- float/visualization/visualizer.py
+++ float/visualization/visualizer.py
@@ -9,7 +9,7 @@ class Visualizer:
     Class for creating plots to visualize information.
 
     """
-    def __init__(self, measures, labels, measure_type, fig_size=(10, 5)):
+    def __init__(self, measures, labels, measure_type):
         """
         Initialize the visualizer using a uniform style.
 
@@ -17,21 +17,20 @@ class Visualizer:
             measures (list[list]): the list of lists of measures to be visualized
             labels (list[str]): the list of labels for the measures
             measure_type (str): the type of the measures passed, one of 'prediction', 'feature_selection, or 'drift_detection'
-            fig_size (float, float): the size of the plots
         """
         self.measures = measures
         self.labels = labels
         self.measure_type = measure_type
-        self.fig_size = fig_size
         self.palette = ['#001219', '#005f73', '#0a9396', '#94d2bd', '#e9d8a6', '#ee9b00', '#ca6702', '#bb3e03', '#ae2012', '#9b2226']
         self.font_size = 12
 
-    def plot(self, plot_title):
+    def plot(self, plot_title, fig_size=(10.2, 5.2)):
         """
         Creates a line plot.
 
         Args:
             plot_title (str): the title of the plot
+            fig_size (float, float):
 
         Returns:
             Axes: the Axes object containing the line plot
@@ -39,7 +38,8 @@ class Visualizer:
         if not self.measure_type == 'prediction':
             warnings.warn(f'Only measures of type "prediction" can be visualized with method plot.')
             return
-        fig, ax = plt.subplots(figsize=self.fig_size)
+
+        fig, ax = plt.subplots(figsize=fig_size)
         for i, (measure, label) in enumerate(zip(self.measures, self.labels)):
             print('here')
             ax.plot(np.arange(len(measure)), measure, color=self.palette[i], label=label)
@@ -49,12 +49,13 @@ class Visualizer:
         plt.title(plot_title)
         return ax
 
-    def scatter(self, plot_title):
+    def scatter(self, plot_title, fig_size=(10, 5)):
         """
         Creates a scatter plot.
 
         Args:
             plot_title (str): the title of the plot
+            fig_size (float, float): the figure size of the plot
 
         Returns:
             Axes: the Axes object(s) containing the scatter plot(s)
@@ -62,22 +63,25 @@ class Visualizer:
         if not self.measure_type == 'prediction':
             warnings.warn(f'Only measures of type "prediction" can be visualized with method scatter.')
             return
-        fig, axes = plt.subplots(len(self.measures), 1, sharex=True, figsize=self.fig_size)
+
+        n_measures = len(self.measures)
+        fig, axes = plt.subplots(n_measures, 1, sharex=True, figsize=fig_size)
         for i, (measure, label) in enumerate(zip(self.measures, self.labels)):
-            ax = axes[i] if len(self.measures) > 1 else axes
+            ax = axes[i] if n_measures > 1 else axes
             ax.scatter(np.arange(len(measure)), measure, color=self.palette[i], label=label)
+            ax.set_xlabel('Time Step $t$', size=self.font_size, labelpad=1.6)
             ax.set_ylabel('Metric Value', size=self.font_size, labelpad=1.6)
             ax.legend()
-        plt.xlabel('Time Step $t$', size=self.font_size, labelpad=1.6)
         plt.suptitle(plot_title)
         return axes
 
-    def bar(self, plot_title):
+    def bar(self, plot_title, fig_size=(10, 5)):
         """
         Creates a bar plot.
 
         Args:
             plot_title (str): the title of the plot
+            fig_size (float, float): the figure size of the plot
 
         Returns:
             Axes: the Axes object containing the bar plot
@@ -85,7 +89,8 @@ class Visualizer:
         if not self.measure_type == 'prediction':
             warnings.warn(f'Only measures of type "prediction" can be visualized with method bar.')
             return
-        fig, ax = plt.subplots(figsize=self.fig_size)
+
+        fig, ax = plt.subplots(figsize=fig_size)
         width = 0.8
         n_measures = float(len(self.measures))
         for i, (measure, label) in enumerate(zip(self.measures, self.labels)):
@@ -96,9 +101,12 @@ class Visualizer:
         plt.title(plot_title)
         return ax
 
-    def draw_selected_features(self):
+    def draw_selected_features(self, fig_size=(10, 5)):
         """
         Draws the selected features at each time step in a scatter plot.
+
+        Args:
+            fig_size (float, float): the figure size of the plot
 
         Returns:
             Axes: the Axes object containing the scatter plot
@@ -106,28 +114,32 @@ class Visualizer:
         if not self.measure_type == 'feature_selection':
             warnings.warn(f'Only measures of type "feature_selection" can be visualized with method draw_selected_features.')
             return
-        x, y = [], []
-        for i, val in enumerate(self.measures):
-            x.extend(np.ones(len(val), dtype=int) * i)
-            y.extend(val)
 
-        fig, ax = plt.subplots(figsize=self.fig_size)
-        ax.grid(True)
-        ax.set_ylabel('Feature Index', size=self.font_size, labelpad=1.5)
-        ax.set_xlabel('Time Step $t$', size=self.font_size, labelpad=1.6)
-        ax.tick_params(axis='both', labelsize=self.font_size * 0.7, length=0)
-        ax.scatter(x, y, marker='.', label='Selected Features', zorder=100, color=self.palette[0])
-        plt.legend(frameon=True, loc='best', fontsize=self.font_size * 0.7, borderpad=0.2, handletextpad=0.2)
-        plt.title('Selected Features At Each Time Step', size=self.font_size)
-        plt.plot()
-        return ax
+        n_measures = len(self.measures)
+        fig, axes = plt.subplots(n_measures, 1, sharex=True, figsize=fig_size)
+        for i, (measure, label) in enumerate(zip(self.measures, self.labels)):
+            x, y = [], []
+            for j, val in enumerate(measure):
+                x.extend(np.ones(len(val), dtype=int) * j)
+                y.extend(val)
 
-    def draw_top_features(self, feature_names):
+            ax = axes[i] if n_measures > 1 else axes
+            ax.grid(True)
+            ax.set_xlabel('Time Step $t$', size=self.font_size, labelpad=1.6)
+            ax.set_ylabel('Feature Index', size=self.font_size, labelpad=1.5)
+            ax.tick_params(axis='both', labelsize=self.font_size * 0.7, length=0)
+            ax.scatter(x, y, marker='.', zorder=100, color=self.palette[i], label=label)
+            ax.legend(frameon=True, loc='best', fontsize=self.font_size * 0.7, borderpad=0.2, handletextpad=0.2)
+        plt.suptitle('Selected Features At Each Time Step', size=self.font_size)
+        return axes
+
+    def draw_top_features(self, feature_names, fig_size=(10, 5)):
         """
         Draws the most selected features over time as a bar plot.
 
         Args:
             feature_names (list): the list of feature names
+            fig_size (float, float): the figure size of the plot
 
         Returns:
             Axes: the Axes object containing the bar plot
@@ -135,19 +147,24 @@ class Visualizer:
         if not self.measure_type == 'feature_selection':
             warnings.warn(f'Only measures of type "feature_selection" can be visualized with method draw_top_features.')
             return
-        n_selected_features = len(self.measures[0])
-        y = [feature for features in self.measures for feature in features]
-        counts = np.bincount(y)
-        top_ftr_idx = counts.argsort()[-n_selected_features:][::-1]
 
-        fig, ax = plt.subplots(figsize=self.fig_size)
-        ax.grid(True, axis='y')
-        ax.bar(np.arange(n_selected_features), counts[top_ftr_idx], width=0.3, zorder=100, color=self.palette[0])
-        ax.set_xticks(np.arange(n_selected_features))
-        ax.set_xticklabels(np.asarray(feature_names)[top_ftr_idx], rotation=20, ha='right')
-        ax.set_ylabel('Times Selected', size=self.font_size, labelpad=1.5)
-        ax.set_xlabel('Top 10 Features', size=self.font_size, labelpad=1.6)
-        ax.tick_params(axis='both', labelsize=self.font_size * 0.7, length=0)
-        ax.set_xlim(-0.2, 9.2)
-        plt.title(f'Top {n_selected_features} Most Selected Features', size=self.font_size)
-        return ax
+        n_measures = len(self.measures)
+        fig, axes = plt.subplots(1, n_measures, sharey=True, figsize=fig_size)
+        for i, (measure, label) in enumerate(zip(self.measures, self.labels)):
+            n_selected_features = len(measure[0])
+            y = [feature for features in measure for feature in features]
+            counts = np.bincount(y)
+            top_ftr_idx = counts.argsort()[-n_selected_features:][::-1]
+
+            ax = axes[i] if n_measures > 1 else axes
+            ax.grid(True, axis='y')
+            ax.bar(np.arange(n_selected_features), counts[top_ftr_idx], width=0.3, zorder=100, color=self.palette[i], label=label)
+            ax.set_xticks(np.arange(n_selected_features))
+            ax.set_xticklabels(np.asarray(feature_names)[top_ftr_idx], rotation=20, ha='right')
+            ax.set_ylabel('Times Selected', size=self.font_size, labelpad=1.5)
+            ax.set_xlabel('Top 10 Features', size=self.font_size, labelpad=1.6)
+            ax.tick_params(axis='both', labelsize=self.font_size * 0.7, length=0)
+            ax.set_xlim(-0.2, 9.2)
+            ax.legend()
+        plt.suptitle(f'Most Selected Features', size=self.font_size)
+        return axes
