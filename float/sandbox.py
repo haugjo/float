1ce91f3242e35306cc55fe56fdc8079b57ab43d3
Effi Tramountani <effi.tramountani@gmail.com>
2021-08-03 10:19:27 +0200

    Add plot which shows known and detected drifts for each drift detector. Update draw_top_features_with_reference to plot all feature selectors in one figure.

--- float/sandbox.py
+++ float/sandbox.py
@@ -1,33 +1,70 @@
-# Here we may test code informally
 from skmultiflow.drift_detection.adwin import ADWIN
 from skmultiflow.neural_networks.perceptron import PerceptronMask
 import matplotlib.pyplot as plt
 from float import *
 
-data_loader = data.DataLoader(None, 'data/datasets/spambase.csv', 0)
-feature_selectors = [feature_selection.ofs.OFS(n_total_features=data_loader.stream.n_features, n_selected_features=10,
-                                               nogueira_window_size=10),
-                     feature_selection.fires.FIRES(n_total_features=data_loader.stream.n_features,
-                                                   n_selected_features=10, classes=data_loader.stream.target_values,
-                                                   nogueira_window_size=10)]
-# adwin = ADWIN(delta=2)
-# concept_drift_detector = concept_drift_detection.SkmultiflowDriftDetector(adwin)
-concept_drift_detector = concept_drift_detection.erics.ERICS(data_loader.stream.n_features)
-perceptron = PerceptronMask()
-predictor = prediction.skmultiflow_perceptron.SkmultiflowPerceptron(perceptron, data_loader.stream.target_values)
 
-for feature_selector in feature_selectors:
-    pipeline.prequential_pipeline.PrequentialPipeline(data_loader, feature_selector, concept_drift_detector,
-                                                      predictor, max_n_samples=data_loader.stream.n_samples).run()
+def get_kdd_conceptdrift_feature_names():
+    return ["duration", "protocol_type", "service", "flag", "src_bytes", "dst_bytes", "land", "wrong_fragment",
+            "urgent", "hot", "num_failed_logins", "logged_in", "num_compromised", "root_shell", "su_attempted",
+            "num_root", "num_file_creations", "num_shells", "num_access_files", "num_outbound_cmds", "is_host_login",
+            "is_guest_login", "count", "srv_count", "serror_rate", "srv_serror_rate", "rerror_rate", "srv_rerror_rate",
+            "same_srv_rate", "diff_srv_rate", "srv_diff_host_rate", "dst_host_count", "dst_host_srv_count",
+            "dst_host_same_srv_rate", "dst_host_diff_srv_rate", "dst_host_same_src_port_rate",
+            "dst_host_srv_diff_host_rate", "dst_host_serror_rate", "dst_host_srv_serror_rate", "dst_host_rerror_rate",
+            "dst_host_srv_rerror_rate"]
+
+
+data_loaders = []
+for file_name in ['spambase', 'adult_conceptdrift', 'har', 'kdd_conceptdrift']:
+    data_loader = data.DataLoader(None, f'data/datasets/{file_name}.csv', 0)
+    data_loaders.append(data_loader)
+
+known_drifts = [[round(data_loader.stream.n_samples * 0.2), round(data_loader.stream.n_samples * 0.4),
+                 round(data_loader.stream.n_samples * 0.6), round(data_loader.stream.n_samples * 0.8)] for data_loader
+                in data_loaders]
+batch_sizes = [10, 50, 10, 100]
+
+pipelines = []
+for i, data_loader in enumerate(data_loaders):
+    feature_names = get_kdd_conceptdrift_feature_names() if i == 3 else data_loader.stream.feature_names
+    feature_selectors = [
+        feature_selection.ofs.OFS(n_total_features=data_loader.stream.n_features, n_selected_features=10,
+                                  nogueira_window_size=10),
+        feature_selection.fires.FIRES(n_total_features=data_loader.stream.n_features,
+                                      n_selected_features=10, classes=data_loader.stream.target_values,
+                                      nogueira_window_size=10)]
+
+    concept_drift_detectors = [concept_drift_detection.SkmultiflowDriftDetector(ADWIN()),
+                               concept_drift_detection.erics.ERICS(data_loader.stream.n_features)]
+    predictor = prediction.skmultiflow_perceptron.SkmultiflowPerceptron(PerceptronMask(),
+                                                                        data_loader.stream.target_values)
+    for feature_selector in feature_selectors:
+        for concept_drift_detector in concept_drift_detectors:
+            prequential_pipeline = pipeline.prequential_pipeline.PrequentialPipeline(data_loader, feature_selector,
+                                                                                     concept_drift_detector,
+                                                                                     predictor,
+                                                                                     batch_size=batch_sizes[i],
+                                                                                     max_n_samples=data_loader.stream.n_samples)
+            prequential_pipeline.run()
+            pipelines.append(prequential_pipeline)
+
+    visualizer = visualization.visualizer.Visualizer(
+        [predictor.accuracy_scores, predictor.precision_scores, predictor.f1_scores, predictor.recall_scores],
+        ['Accuracy', 'Precision', 'F1', 'Recall'],
+        'prediction')
+    visualizer.plot(plot_title='Predictor')
+    plt.show()
+
+    visualizer = visualization.visualizer.Visualizer(
+        [feature_selectors[0].selection, feature_selectors[1].selection], ['OFS', 'FIRES'], 'feature_selection')
+    print('DATA FEATURES:', feature_names)
+    visualizer.draw_top_features_with_reference(feature_names, fig_size=(15, 5))
+    plt.show()
+    visualizer.draw_selected_features(layout=(2, 1), fig_size=(10, 8))
+    plt.show()
 
 visualizer = visualization.visualizer.Visualizer(
-    [predictor.accuracy_scores, predictor.precision_scores, predictor.f1_scores, predictor.recall_scores], ['Accuracy', 'Precision', 'F1', 'Recall'],
-    'prediction')
-visualizer.scatter(plot_title='Predictor', layout=(2, 2))
-plt.show()
-
-visualizer = visualization.visualizer.Visualizer([feature_selectors[0].selection, feature_selectors[1].selection], ['OFS', 'FIRES'], 'feature_selection')
-visualizer.draw_top_features_with_reference(data_loader.stream.feature_names, layout=(1, 2), fig_size=(15, 5))
-plt.show()
-visualizer.draw_selected_features(layout=(2, 1), fig_size=(10, 8))
-plt.show()
+    [concept_drift_detectors[0].global_drifts, concept_drift_detectors[1].global_drifts], ['ERICS'], 'drift_detection')
+visualizer.draw_concept_drifts([data_loader.stream for data_loader in data_loaders], known_drifts, batch_sizes,
+                               ['spambase', 'adult', 'har', 'kdd'], ['adwin', 'erics'])
--- float/visualization/visualizer.py
+++ float/visualization/visualizer.py
@@ -21,7 +21,8 @@ class Visualizer:
         self.measures = measures
         self.labels = labels
         self.measure_type = measure_type
-        self.palette = ['#001219', '#005f73', '#0a9396', '#94d2bd', '#e9d8a6', '#ee9b00', '#ca6702', '#bb3e03', '#ae2012', '#9b2226']
+        self.palette = ['#001219', '#005f73', '#0a9396', '#94d2bd', '#e9d8a6', '#ee9b00', '#ca6702', '#bb3e03',
+                        '#ae2012', '#9b2226']
         self.font_size = 12
 
     def plot(self, plot_title, fig_size=(10.2, 5.2)):
@@ -41,7 +42,6 @@ class Visualizer:
 
         fig, ax = plt.subplots(figsize=fig_size)
         for i, (measure, label) in enumerate(zip(self.measures, self.labels)):
-            print('here')
             ax.plot(np.arange(len(measure)), measure, color=self.palette[i], label=label)
         ax.set_xlabel('Time Step $t$', size=self.font_size, labelpad=1.6)
         ax.set_ylabel('Metric Value', size=self.font_size, labelpad=1.6)
@@ -75,8 +75,9 @@ class Visualizer:
         fig, axes = plt.subplots(layout[0], layout[1], figsize=fig_size, sharex=share_x, sharey=share_y)
         for i in range(layout[0]):
             for j in range(layout[1]):
-                ax = axes if n_measures == 1 else (axes[i+j] if layout[0] == 1 or layout[1] == 1 else axes[i, j])
-                ax.scatter(np.arange(len(self.measures[i+j])), self.measures[i+j], color=self.palette[i+j], label=self.labels[i+j])
+                ax = axes if n_measures == 1 else (axes[i + j] if layout[0] == 1 or layout[1] == 1 else axes[i, j])
+                ax.scatter(np.arange(len(self.measures[i + j])), self.measures[i + j], color=self.palette[i + j],
+                           label=self.labels[i + j])
                 ax.set_xlabel('Time Step $t$', size=self.font_size, labelpad=1.6)
                 ax.set_ylabel('Metric Value', size=self.font_size, labelpad=1.6)
                 ax.legend()
@@ -102,7 +103,8 @@ class Visualizer:
         width = 0.8
         n_measures = float(len(self.measures))
         for i, (measure, label) in enumerate(zip(self.measures, self.labels)):
-            ax.bar(np.arange(len(measure)) - width / 2. + i / n_measures * width, measure, width=width / n_measures, align="edge", color=self.palette[i], label=label)
+            ax.bar(np.arange(len(measure)) - width / 2. + i / n_measures * width, measure, width=width / n_measures,
+                   align="edge", color=self.palette[i], label=label)
         ax.set_xlabel('Time Step $t$', size=self.font_size, labelpad=1.6)
         ax.set_ylabel('Metric Value', size=self.font_size, labelpad=1.6)
         plt.legend()
@@ -123,7 +125,8 @@ class Visualizer:
             Axes: the Axes object containing the scatter plot
         """
         if not self.measure_type == 'feature_selection':
-            warnings.warn(f'Only measures of type "feature_selection" can be visualized with method draw_selected_features.')
+            warnings.warn(
+                f'Only measures of type "feature_selection" can be visualized with method draw_selected_features.')
             return
 
         n_measures = len(self.measures)
@@ -135,7 +138,7 @@ class Visualizer:
         for i in range(layout[0]):
             for j in range(layout[1]):
                 x, y = [], []
-                for k, val in enumerate(self.measures[i+j]):
+                for k, val in enumerate(self.measures[i + j]):
                     x.extend(np.ones(len(val), dtype=int) * k)
                     y.extend(val)
 
@@ -144,7 +147,7 @@ class Visualizer:
                 ax.set_xlabel('Time Step $t$', size=self.font_size, labelpad=1.6)
                 ax.set_ylabel('Feature Index', size=self.font_size, labelpad=1.5)
                 ax.tick_params(axis='both', labelsize=self.font_size * 0.7, length=0)
-                ax.scatter(x, y, marker='.', zorder=100, color=self.palette[i+j], label=self.labels[i+j])
+                ax.scatter(x, y, marker='.', zorder=100, color=self.palette[i + j], label=self.labels[i + j])
                 ax.legend(frameon=True, loc='best', fontsize=self.font_size * 0.7, borderpad=0.2, handletextpad=0.2)
         plt.suptitle('Selected Features At Each Time Step', size=self.font_size)
         return axes
@@ -175,8 +178,8 @@ class Visualizer:
         fig, axes = plt.subplots(layout[0], layout[1], figsize=fig_size, sharex=share_x, sharey=share_y)
         for i in range(layout[0]):
             for j in range(layout[1]):
-                n_selected_features = len(self.measures[i+j][0])
-                y = [feature for features in self.measures[i+j] for feature in features]
+                n_selected_features = len(self.measures[i + j][0])
+                y = [feature for features in self.measures[i + j] for feature in features]
                 counts = [(x, y.count(x)) for x in np.unique(y)]
                 top_features = sorted(counts, key=lambda x: x[1])[-n_selected_features:][::-1]
                 top_features_idx = [x[0] for x in top_features]
@@ -184,7 +187,8 @@ class Visualizer:
 
                 ax = axes if n_measures == 1 else (axes[i + j] if layout[0] == 1 or layout[1] == 1 else axes[i, j])
                 ax.grid(True, axis='y')
-                ax.bar(np.arange(n_selected_features), top_features_vals, width=0.3, zorder=100, color=self.palette[i+j], label=self.labels[i+j])
+                ax.bar(np.arange(n_selected_features), top_features_vals, width=0.3, zorder=100,
+                       color=self.palette[i + j], label=self.labels[i + j])
                 ax.set_xticks(np.arange(n_selected_features))
                 ax.set_xticklabels(np.asarray(feature_names)[top_features_idx], rotation=20, ha='right')
                 ax.set_ylabel('Times Selected', size=self.font_size, labelpad=1.5)
@@ -195,16 +199,13 @@ class Visualizer:
         plt.suptitle(f'Most Selected Features', size=self.font_size)
         return axes
 
-    def draw_top_features_with_reference(self, feature_names, layout, fig_size=(10, 5), share_x=True, share_y=True):
+    def draw_top_features_with_reference(self, feature_names, fig_size=(10, 5)):
         """
         Draws the most selected features over time as a bar plot.
 
         Args:
             feature_names (list): the list of feature names
-            layout (int, int): the layout of the figure (nrows, ncols)
             fig_size (float, float): the figure size of the plot
-            share_x (bool): True if the x axis should be shared among plots in the figure, False otherwise
-            share_y (bool): True if the y axis among plots in the figure, False otherwise
 
         Returns:
             Axes: the Axes object containing the bar plot
@@ -213,45 +214,70 @@ class Visualizer:
             warnings.warn(f'Only measures of type "feature_selection" can be visualized with method draw_top_features.')
             return
 
+        width = 0.8
         n_measures = len(self.measures)
-        if layout[0] * layout[1] < n_measures:
-            warnings.warn('The number of measures cannot be plotted in such a layout.')
+
+        fig, ax = plt.subplots(figsize=fig_size)
+        for i, (measure, label) in enumerate(zip(self.measures, self.labels)):
+            n_selected_features = len(measure[0])
+            y = [feature for features in measure for feature in features]
+            counts = [(x, y.count(x)) for x in np.unique(y)]
+            if i == 0:
+                top_features = sorted(counts, key=lambda x: x[1])[-n_selected_features:][::-1]
+                top_features_idx = [x[0] for x in top_features]
+                top_features_vals = [x[1] for x in top_features]
+            else:
+                top_features_vals = [dict(counts)[x] if x in dict(counts).keys() else 0 for x in top_features_idx]
+                print(
+                    f"Top {label} features not in reference {self.labels[0]}: {np.asarray(feature_names)[[x for x in top_features_idx if x not in dict(counts).keys()]]}")
+
+            ax.grid(True, axis='y')
+            ax.bar(np.arange(n_selected_features) - width / 2. + i / n_measures * width, top_features_vals,
+                   width=width / n_measures, zorder=100, color=self.palette[i], label=label)
+        plt.xticks(np.arange(n_selected_features), labels=np.asarray(feature_names)[top_features_idx], rotation=20, ha='right')
+        plt.ylabel('Times Selected', size=self.font_size, labelpad=1.5)
+        plt.xlabel('Top 10 Features', size=self.font_size, labelpad=1.6)
+        plt.tick_params(axis='both', labelsize=self.font_size * 0.7, length=0)
+        plt.legend()
+        plt.title(f'Most Selected Features', size=self.font_size)
+        return ax
+
+    def draw_concept_drifts(self, data, known_drifts, batch_sizes, data_labels, cdd_labels):
+        if not self.measure_type == 'drift_detection':
+            warnings.warn(f'Only measures of type "drift_detection" can be visualized with method draw_concept_drifts.')
             return
 
-        fig, axes = plt.subplots(layout[0], layout[1], figsize=fig_size, sharex=share_x, sharey=share_y)
-        for i in range(layout[0]):
-            for j in range(layout[1]):
-                n_selected_features = len(self.measures[i+j][0])
-                y = [feature for features in self.measures[i+j] for feature in features]
-                counts = [(x, y.count(x)) for x in np.unique(y)]
-                if i+j == 0:
-                    top_features = sorted(counts, key=lambda x: x[1])[-n_selected_features:][::-1]
-                    top_features_idx = [x[0] for x in top_features]
-                    top_features_vals = [x[1] for x in top_features]
+        drifts = []
+        for i, (stream, known_drift, batch_size, data_label) in enumerate(
+                zip(data, known_drifts, batch_sizes, data_labels)):
+            drifts.append([])
+            for measure, cdd_label in zip(self.measures, cdd_labels):
+                drifts[i].append((cdd_label, np.asarray(measure) * batch_size + batch_size))
+
+            fig = plt.figure(figsize=(10, 5))
+            y_loc = 0  # y-coordinate of scatter points
+
+            # Draw known drifts
+            for true in known_drift:
+                if isinstance(true, tuple):
+                    plt.axvspan(true[0], true[1], facecolor='#eff3ff', edgecolor='#9ecae1', hatch="//")
                 else:
-                    top_features_vals = [dict(counts)[x] if x in dict(counts).keys() else 0 for x in top_features_idx]
-                    print(f"Top {self.labels[i+j]} features not in reference {self.labels[0]}: {np.asarray(feature_names)[[x for x in top_features_idx if x not in dict(counts).keys()]]}")
+                    plt.axvline(true, color='#9ecae1', lw=3, zorder=0)
 
-                ax = axes if n_measures == 1 else (axes[i + j] if layout[0] == 1 or layout[1] == 1 else axes[i, j])
-                ax.grid(True, axis='y')
-                ax.bar(np.arange(n_selected_features), top_features_vals, width=0.3, zorder=100, color=self.palette[i+j], label=self.labels[i+j])
-                ax.set_xticks(np.arange(n_selected_features))
-                ax.set_xticklabels(np.asarray(feature_names)[top_features_idx], rotation=20, ha='right')
-                ax.set_ylabel('Times Selected', size=self.font_size, labelpad=1.5)
-                ax.set_xlabel('Top 10 Features', size=self.font_size, labelpad=1.6)
-                ax.tick_params(axis='both', labelsize=self.font_size * 0.7, length=0)
-                ax.set_xlim(-0.2, 9.2)
-                ax.legend()
-        plt.suptitle(f'Most Selected Features', size=self.font_size)
-        return axes
+            # Draw detected drifts
+            y_tick_labels = []
 
-    def draw_concept_drifts(self, known_drifts):
-        """
+            for model in drifts[i]:
+                plt.axhline(y_loc, color='black', zorder=5)
+                plt.scatter(model[1], np.repeat(y_loc, len(model[1])), marker='|', color='black', s=300, zorder=10)
+                y_loc += 0.5
+                y_tick_labels.append(model[0].upper())  # save model name
 
-        Args:
-            known_drifts (dict):
-
-        Returns:
-
-        """
-        raise NotImplementedError
+            plt.yticks(np.arange(0, 1, 0.5), y_tick_labels, fontsize=12)
+            tx = np.arange(0, stream.n_samples - 10, round(stream.n_samples * 0.1))
+            plt.xticks(tx, tx, fontsize=12)
+            plt.xlim(-stream.n_samples * 0.005,
+                     stream.n_samples + stream.n_samples * 0.005)
+            plt.xlabel('# Observations', fontsize=14)
+            plt.title(data_label)
+            plt.show()
